886bc80319668c836174267ebc68d7454cd9a083
yusuke mitsui <funnyfool14@gmail.com>
2021-02-06 23:45:32 +0000

    ‘up’

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -21,37 +21,42 @@ class MessageController extends Controller
     
     public function show($user_id)
     {
-        $chat_id=0;
+        //$chat_id=0;
         $me=\Auth::user();
         $my_id=\Auth::id();
         $reciever=User::findOrFail($user_id);
+        
+        
 
         //自分と紐づくチャット一覧を取得
         $chats=$me->chats;
-        foreach($chats as $chat){
-            //dd($chat);
+        $chat=$chats->where('id',$user_id);
+        dd($chat);
+        /*foreach($chats as $chat){
             //それぞれのチャットのユーザを配列にして取り出す
-            $ids=$chat->users()->pluck('user_id')->toArray();
+            $ids=$chat->users->pluck('id')->toArray();
+            //$ids=$chat->users()->pluck('user_id')->toArray();
             //$my_idと$user_idの配列の組み合わせがあれば
             if(in_array($my_id,$ids)&&in_array($user_id,$ids)){
                 //その組み合わせのチャットのidを取り出す
-                $chat_id=$chat->id
+                $chat_id=$chat->id;
             ;}
             
-            if($chat_id){
+            if(isset($chat_id)){
                 $chat=Chat::find($chat_id);
             }
             //なければ
             else{
                 //インスタンスを生成し
-                $chat=new Chat;
-                $chat->save();
+                $new_chat=new Chat;
+                $new_chat->save();
                 //生成したチャットのインスタンスからidを取得
-                $chat_id=$chat->id;
+                $chat_id=$new_chat->id;
                 //自分と相手のidを中間テーブルにレコード
-                $chat->users()->sync([$my_id,$user_id]);
+                $new_chat->users()->sync([$my_id,$user_id]);
             }
-        }
+        }:
+        */
         //chat_idでメッセージを呼び出す
         $messages=Message::where('chat_id',$chat_id);
         
