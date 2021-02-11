9c733852281ed8609f5e8ba6959fa2e0674c2d2e
yusuke mitsui <funnyfool14@gmail.com>
2021-02-11 07:57:37 +0000

    ‘chat生成’

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -8,6 +8,7 @@ use App\User;
 use App\Chat;
 //これがないと”Auth::"が使えない
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class MessageController extends Controller
 {
@@ -26,43 +27,28 @@ class MessageController extends Controller
         $my_id=\Auth::id();
         $reciever=User::findOrFail($user_id);
         
+        //DB::table('chats_users')->where('user_id',$user_id)->get('chat_id')->toArray()を無名関数で呼び出し
+        //相手の紐づくchat_idを取得
+        $chat_id=DB::table('chats_users')->whereIn('chat_id', function ($query) use ($user_id) {
+            $query->select('chat_id')
+                ->from('chats_users')
+                ->where('user_id', $user_id);
+        //その中から自分に紐づくcaht_idを取得し$chatとして定義
+        })->where('user_id', $my_id)->first('chat_id');
         
-
-        //自分と紐づくチャット一覧を取得
-        $my_chats=$me->chats;
-        $chats_users=$my_chats->users->toArray();
-        
-        if(inArray($chats_users,$user_id)) {
-            ;
+        //$chatがある時
+        /*if(isset($chat)){
+            $chat_id=$chat->id;
         }
-        else{
-            ;
+        //$chatがない時
+        else{*/if(is_null($chat_id)){
+            $chat=new Chat;
+            $chat->save();
+            //生成したチャットのインスタンスからidを取得
+            $chat_id=$chat->id;
+            //自分と相手のidを中間テーブルにレコード
+            $chat->users()->sync([$my_id,$user_id]);
         }
-        /*foreach($chats as $chat){
-            //それぞれのチャットのユーザを配列にして取り出す
-            $ids=$chat->users->pluck('id')->toArray();
-            //$ids=$chat->users()->pluck('user_id')->toArray();
-            //$my_idと$user_idの配列の組み合わせがあれば
-            if(in_array($my_id,$ids)&&in_array($user_id,$ids)){
-                //その組み合わせのチャットのidを取り出す
-                $chat_id=$chat->id;
-            ;}
-            
-            if(isset($chat_id)){
-                $chat=Chat::find($chat_id);
-            }
-            //なければ
-            else{
-                //インスタンスを生成し
-                $new_chat=new Chat;
-                $new_chat->save();
-                //生成したチャットのインスタンスからidを取得
-                $chat_id=$new_chat->id;
-                //自分と相手のidを中間テーブルにレコード
-                $new_chat->users()->sync([$my_id,$user_id]);
-            }
-        }:
-        */
         //chat_idでメッセージを呼び出す
         $messages=Message::where('chat_id',$chat_id);
         
