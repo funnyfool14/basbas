b23e4749c5ded1d43e3dbfceb7cb309e6cc715b3
三井友輔 <mitsui@mitsuitomotasukunoMacBook-Pro.local>
2022-03-22 12:10:18 +0900

    message.index

--- resources/views/users/button.blade.php
+++ resources/views/users/button.blade.php
@@ -1,6 +1,7 @@
 
 <aside class="col-sm-4">                    
     @if($user!=Auth::user())
+        @if(Auth::user()->is_friend($user->id))
         {{--リクエストを送る--}}
         <div class="mt-2">
             {!!link_to_route('request.friend','request',['id'=>$user->id],['class'=>'btn btn-outline-success btn-block'])!!}
@@ -12,6 +13,16 @@
         {{---フレンド一覧--}}
         <div class="mt-2">
             {!!link_to_route('friend.index','friends',['id'=>$user->id],['class'=>'btn btn-outline-primary btn-block'])!!}
-        </div>
+        </div>                    
+        @else
+            {{--メッセージ--}}
+            <div class="mt-2">
+                {!!link_to_route('messages.show','message',['id'=>$user->id],['class'=>'btn btn-outline-success btn-lg btn-block'])!!}
+            </div>
+            {{---フレンド一覧--}}
+            <div class="mt-2">
+                {!!link_to_route('friend.index','friends',['id'=>$user->id],['class'=>'btn btn-outline-primary btn-lg btn-block'])!!}
+            </div>
+        @endif
     @endif
 </aside>
