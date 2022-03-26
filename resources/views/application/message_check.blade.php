e1792e909b47dabf5654eff1598efb283cc6c81c
三井友輔 <mitsui@mitsuitomotasukunoMacBook-Pro.local>
2022-03-26 12:57:33 +0900

    applycation.show

--- resources/views/application/message_check.blade.php
+++ resources/views/application/message_check.blade.php
@@ -1,13 +1,13 @@
 @if(($message->check)==0)
-    <form method='POST' action='{{route('application.message_check',['message'=>$message->id])}}' enctype='multipart/form-data'>
-        @csrf
-        @method('put')
-        <button type="submit" class='btn btn-outline-danger btn-sm'>要チェック</button>
-    </form>
+<form method='POST' action='{{route('application.message_check',['message'=>$message->id])}}' enctype='multipart/form-data'>
+    @csrf
+    @method('put')
+    <button type="submit" class='btn btn-outline-danger btn-sm'>要チェック</button>
+</form>
 @elseif(($message->check)!=0)
-    <form method='POST' action='{{route('application.message_recheck',['message'=>$message->id])}}' enctype='multipart/form-data'>
-        @csrf
-        @method('put')
-        <button type="submit" class='btn btn-outline-success btn-sm'>チェック済</button>
-    </form>    
+<form method='POST' action='{{route('application.message_recheck',['message'=>$message->id])}}' enctype='multipart/form-data'>
+    @csrf
+    @method('put')
+    <button type="submit" class='btn btn-outline-success btn-sm'>チェック済</button>
+</form>    
 @endif   
--- resources/views/application/show.blade.php
+++ resources/views/application/show.blade.php
@@ -2,29 +2,24 @@
 @section('content')
 {{--メッセージ送信ボタンと申し込みボタン--}}
     {{--メッセージボタンでメッセージ送信--}} 
-    <div class='row'>
-        <h1 class='mb-5'>{{link_to_route('team.show',$team->name,[$team->id],[])}}</h1>
-        <div class='offset-sm-1 col-sm-4'>
-            @if($team->applicant())
-                @if(($application->apply())==0)
-                    <form method='POST' action='{{route('application.request',['id'=>$connect_id])}}' enctype='multipart/form-data'>
-                        @csrf
-                        @method('PUT')
-                        <div class=''>
-                            <button type="submit" class='btn btn-outline-primary btn-block btn-lg'>入部を申し込む</button>
-                        </div>
-                    </form>
-                @elseif(($application->apply())==1)
-                    <form method='POST' action='{{route('application.request',['id'=>$connect_id])}}' enctype='multipart/form-data'>
-                        @csrf
-                        @method('PUT')
-                        <div class=''>
-                            <button type="submit" class='btn btn-outline-danger btn-block btn-lg'>申し込みのキャンセル</button>
-                        </div>
-                    </form>
-                @endif
+<div>    
+    <h1 class='mb-5'>{{link_to_route('team.show',$team->name,[$team->id],[])}}</h1>
+    <div class='offset-sm-1 col-sm-4'>
+        @if($team->applicant())
+            @if(($application->apply())==0)
+            <form method='POST' action='{{route('application.request',['id'=>$connect_id])}}' enctype='multipart/form-data'>
+                @csrf
+                @method('PUT')
+                <button type="submit" class='btn btn-outline-primary btn-block btn-lg'>入部を申し込む</button>
+            </form>
+            @elseif(($application->apply())==1)
+            <form method='POST' action='{{route('application.request',['id'=>$connect_id])}}' enctype='multipart/form-data'>
+                @csrf
+                @method('PUT')
+                <button type="submit" class='btn btn-outline-danger btn-block btn-lg'>申し込みのキャンセル</button>
+            </form>
             @endif
-        </div>
+        @endif
     </div>
     <form method='POST' action='{{route('application.message',['application'=>$connect_id])}}' enctype='multipart/form-data'>
         @csrf
@@ -38,53 +33,54 @@
             </div>
         </div>    
     </form>
-    <div class=''>
-        @foreach($messages as $message)
-            <div class='mt-5'>
-                @if($message->applicant())
-                    <div class="text-right">
-                        <div class='row'>
-             	            <h3 class='col-sm-10'>{!!nl2br(e($message->message))!!}</h3>
-             	            {{--メッセージ送信者に削除ボタン--}}
-             	            <div class='col-sm-2'>
-                 	            @if(($message->user_id)==Auth::id())
-                                    <form method='POST' action='{{route('application.message_delete',['message'=>$message->id])}}' enctype='multipart/form-data'>
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class='btn btn-outline-danger btn-sm'>delete</button>
-                                    </form>
-                                {{--キャプテンと副キャプテンに既読チェックボタン--}}    
-                                @elseif(($team->captain())==Auth::user())
-                                    @include('application.message_check')
-                                @elseif($team->introduction())
-                                    @if(($team->deputy())==Auth::user())
-                                        @include('application.message_check')
-                                    @endif
+</div>
+<div class=''>
+    @foreach($messages as $message)
+        <div class='mt-5'>
+            @if($message->applicant())
+                <div class="text-right">
+                    <div class='row'>
+                        <h3 class='col-sm-10'>{!!nl2br(e($message->message))!!}</h3>
+                        {{--メッセージ送信者に削除ボタン--}}
+                        <div class='col-sm-2'>
+                            @if(($message->user_id)==Auth::id())
+                            <form method='POST' action='{{route('application.message_delete',['message'=>$message->id])}}' enctype='multipart/form-data'>
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class='btn btn-outline-danger btn-sm'>delete</button>
+                            </form>
+                            {{--キャプテンと副キャプテンに既読チェックボタン--}}    
+                            @elseif(($team->captain())==Auth::user())
+                            @include('application.message_check')
+                            @elseif($team->introduction())
+                                @if(($team->deputy())==Auth::user())
+                                @include('application.message_check')
                                 @endif
-                            </div>
-                        </div>    
-                 	    <h4 class='mr-5'>{{$message->user()->firstName.' '.$message->user()->lastName}}</h4>
-	                </div>
-    	        @else
-	                <div class='text-left'>
-                        <div class='row'>
-             	            @if(($message->user_id)==Auth::id())
-                                <div class='form-group col-sm-1 '>
-                                    <form method='POST' action='{{route('application.message_delete',['message'=>$message->id])}}' enctype='multipart/form-data'>
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class='btn btn-outline-danger btn-sm btn-block'>delete</button>
-                                    </form>
-                                </div>
-                                @else
-                                <div class='offset-sm-1'>{{''}}</div>
                             @endif
-             	            <h3 class='offset-sm-1'>{!!nl2br(e($message->message))!!}</h3>
-                        </div>   
-                 	    <h4 class='ml-5'>{{$message->user()->firstName.' '.$message->user()->lastName}}</h4>
-	                </div>
-    	       @endif
-    	   </div>
-    	@endforeach
-	</div>       
+                        </div>
+                    </div>    
+                    <h4 class='mr-5'>{{$message->user()->firstName.' '.$message->user()->lastName}}</h4>
+                </div>
+            @else
+                <div class='text-left'>
+                    <div class='row'>
+                        @if(($message->user_id)==Auth::id())
+                        <div class='form-group col-sm-1 '>
+                            <form method='POST' action='{{route('application.message_delete',['message'=>$message->id])}}' enctype='multipart/form-data'>
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class='btn btn-outline-danger btn-sm btn-block'>delete</button>
+                            </form>
+                        </div>
+                        @else
+                        <div class='offset-sm-1'>{{''}}</div>
+                        @endif
+                        <h3 class='offset-sm-1'>{!!nl2br(e($message->message))!!}</h3>
+                    </div>   
+                    <h4 class='ml-5'>{{$message->user()->firstName.' '.$message->user()->lastName}}</h4>
+                </div>
+            @endif
+        </div>
+    @endforeach
+</div>       
 @endsection('content')
