058668eb20341cbc51cd1a15ec1f610014c6a82a
三井友輔 <mitsui@mitsuitomotasukunoMacBook-Pro.local>
2022-03-27 15:24:51 +0900

    hoop.show

--- resources/views/hoops/show.blade.php
+++ resources/views/hoops/show.blade.php
@@ -3,6 +3,6 @@
 <div class = "text-center">
     <h2 class ="mt-5">{{$hoop->name}}</h2>
     <h4 class ="mt-5">{{'@'.$hoop->adress}}</h4>
-    <h4 class ="mt-5">{{$hoop->detail1}}</h4>
+    <h4 class ="mt-5">{!!nl2br(e($hoop->detail1))!!}</h4>
 </div>
 @endsection('content')
